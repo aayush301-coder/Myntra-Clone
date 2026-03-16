3c2feaa2319d136e0ba5ae34607710e730dec644
aayush301-coder <aayushgarg301@gmail.com>
2026-03-16 22:44:39 +0530

    Add search focus animation

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ this.classList.add("active");
 
 });
 });
+
+const searchInput = document.querySelector(".search_input");
+const searchBar = document.querySelector(".search_bar");
+
+searchInput.addEventListener("focus", () => {
+searchBar.style.boxShadow = "0 0 0 2px #ff3f6c33";
+});
+
+searchInput.addEventListener("blur", () => {
+searchBar.style.boxShadow = "none";
+});
