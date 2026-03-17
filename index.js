79d1d2062035ea1fbe3afcb2dae84bee66fa2b6c
aayush301-coder <aayushgarg301@gmail.com>
2026-03-17 22:56:02 +0530

    Default Reveal Style

--- index.js
+++ index.js
@@ -123,3 +123,11 @@ el.style.transform = "translateY(0)";
 });
 
 });
+
+revealElements.forEach(el => {
+
+el.style.opacity = "0";
+el.style.transform = "translateY(40px)";
+el.style.transition = "all 0.6s ease";
+
+});
